22dcae97826da74cd0fc1940600e739f38719a9f
Carlos Almeida <carlpaivalm@yahoo.com>
2017-09-04 15:38:35 +0100

    Added model/view structure and classes

---
+++ pokedex4/Model/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex4
+//
+//  Created by Carlos Almeida on 04/09/17.
+//  Copyright © 2017 weHD. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ pokedex4/View/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  pokedex4
+//
+//  Created by Carlos Almeida on 04/09/17.
+//  Copyright © 2017 weHD. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
